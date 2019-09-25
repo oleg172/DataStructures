c27e44e81bf982a9f3e95019dffccc2f92ffa940
olmi0615 <oleg.miliaev@netcracker.com>
2019-09-25 18:08:32 +0300

    add new structure arrayList

---
+++ src/main/java/data/structure/arrayList/ArrayList.java
@@ -0,0 +1,179 @@
+package data.structure.arrayList;
+
+public class ArrayList<T extends Comparable> {
+
+    private Object[] items;
+    private int count;
+
+    /**
+     * Инициализация коллекции
+     */
+    public ArrayList() {
+        items = new Object[0];
+    }
+
+    /**
+     * Инциализация коллекции размером size.
+     * Если передоваемый параметр size < 0, выбрасывается исключение IllegalArgumentException
+     *
+     * @param size - размер коллекции
+     */
+    public ArrayList(int size) {
+        if (size < 0) {
+            throw new IllegalArgumentException("Size of array should be more then 0");
+        }
+        items = new Object[size];
+    }
+
+    /**
+     * Создает новый массива по формуле (size * 3) / 2 + 1
+     * и копирует значения из старого массива в новый
+     */
+    private void growArray() {
+        int newLength = (items.length * 3) / 2 + 1;
+        Object[] newArray = new Object[newLength];
+        System.arraycopy(items, 0, newArray, 0, items.length);
+        items = newArray;
+    }
+
+    /**
+     * Вставляет значение на заданную позицию в коллекции,
+     * сдвигая элементы находящиеся правее вправо.
+     * Если index больше размера массива, выбрасывается исключение IllegalArgumentException.
+     *
+     * @param index - позиция на которую будет вставлен новый элемент
+     * @param item  - добавляемый элемент
+     */
+    public void insert(int index, T item) {
+        if (index > items.length) {
+            throw new IllegalArgumentException("Index should be less than array size. " +
+                    "Array size = " + items.length + "; index = " + index);
+        }
+        if (items.length == count) {
+            growArray();
+        }
+        System.arraycopy(items, index, items, index + 1, count - index);
+        items[index] = item;
+        count++;
+    }
+
+    /**
+     * Вставляет элемент в конец списка.
+     * Если размер массива равен количеству элементов в нем, то увеличиваем размер массива.
+     *
+     * @param item - добавляемый элемент
+     */
+    public void add(T item) {
+        if (items.length == count) {
+            growArray();
+        }
+        items[count] = item;
+
+        count++;
+    }
+
+    /**
+     * Элемент на позиции index удаляется из массива, сдвигая элменеты находящиеся правее него влево
+     *
+     * @param index - позиция с которой будет удален элемент
+     */
+    public void removeAt(int index) {
+        if (index >= count) {
+            throw new IndexOutOfBoundsException();
+        }
+        int shiftStart = index + 1;
+        if (shiftStart < count) {
+            System.arraycopy(items, shiftStart, items, index, count - shiftStart);
+        }
+        count--;
+    }
+
+    /**
+     * Удаляет первое вхождение элемента item из коллекции
+     *
+     * @param item - удаляемый элемент
+     * @return true если объект был удален, иначе false
+     */
+    public boolean remove(T item) {
+        for (int i = 0; i < count; i++) {
+            if (items[i].equals(item)) {
+                removeAt(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Возвращает позицию первого вхождения элемента item в коллекции
+     *
+     * @param item - искомый элемент
+     * @return первую позицию на которой встерчается элемент item.
+     * Если такого элемента нет, то возвращается -1
+     */
+    public int indexOf(T item) {
+        for (int i = 0; i < count; i++) {
+            if (items[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Возвращает элемент из коллекции на позиции index
+     *
+     * @param index - позиция элемента в массиве
+     * @return элемент на позиции index,
+     * если index больше количества элементов в массиве выбрасывается исключение IllegalArgumentException
+     */
+    public T getElement(int index) {
+        if (index < count) {
+            return (T) items[index];
+        }
+        throw new IllegalArgumentException("Index should be less than array size. " +
+                "Array size = " + count + "; index = " + index);
+    }
+
+    /**
+     * Проверяет наличие элемента item в коллекции
+     *
+     * @param item - искомый элемент
+     * @return возвращает true если элемент находится в массиве, иначе false
+     */
+    public boolean contains(T item) {
+        return indexOf(item) != -1;
+    }
+
+    /**
+     * Удаляет все элементы из коллекции
+     */
+    public void clear() {
+        items = new Object[0];
+        count = 0;
+    }
+
+    /**
+     * Копирует элементы из ArrayList в заданный массив
+     *
+     * @param array - массив в который копируются элементы ArrayList
+     * @param index - позиция с которой необходимо начать вставлять элементы в коллекцию array
+     */
+    public void copyTo(T[] array, int index) {
+        System.arraycopy(items, 0, array, index, count);
+    }
+
+    /**
+     * @return количество элементов в  коллекции
+     */
+    public int getCount() {
+        return count;
+    }
+
+    /**
+     * @return возвращает массив элементов коллекции
+     */
+    public Object[] toArray() {
+        return items;
+    }
+}
---
+++ src/test/java/data/structure/TestArrayList.java
@@ -0,0 +1,161 @@
+package data.structure;
+
+import data.structure.arrayList.ArrayList;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestArrayList {
+
+    private ArrayList<Integer> list;
+
+    @Before
+    public void setUp() {
+        list = new ArrayList<Integer>(4);
+        list.add(0);
+        list.add(1);
+        list.add(0);
+        list.add(2);
+    }
+
+    @Test
+    public void testDefaultInitialization() {
+        list = new ArrayList<Integer>();
+        Assert.assertEquals(0, list.getCount());
+        Assert.assertEquals(0, list.toArray().length);
+    }
+
+    @Test
+    public void testInitialization() {
+        list = new ArrayList<Integer>(10);
+        Assert.assertEquals(0, list.getCount());
+        Assert.assertEquals(10, list.toArray().length);
+    }
+
+    @Test
+    public void testInsertInPosition() {
+        list = new ArrayList<Integer>(2);
+        list.insert(0, 1);
+        list.insert(1, 2);
+        Assert.assertEquals(2, list.getCount());
+        Assert.assertEquals(Integer.valueOf(1), list.getElement(0));
+        Assert.assertEquals(Integer.valueOf(2), list.getElement(1));
+    }
+
+    @Test
+    public void testInsertInPositionShift() {
+        list = new ArrayList<Integer>(2);
+        list.insert(0, 1);
+        list.insert(1, 2);
+        list.insert(0, 3);
+        Assert.assertEquals(3, list.getCount());
+        Assert.assertEquals(Integer.valueOf(3), list.getElement(0));
+        Assert.assertEquals(Integer.valueOf(1), list.getElement(1));
+        Assert.assertEquals(Integer.valueOf(2), list.getElement(2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInsertThrowException() {
+        list.insert(5, 1);
+    }
+
+    @Test
+    public void testAdd() {
+        list = new ArrayList<Integer>(2);
+        list.add(0);
+        list.add(1);
+        Assert.assertEquals(2, list.getCount());
+        Assert.assertEquals(Integer.valueOf(0), list.getElement(0));
+        Assert.assertEquals(Integer.valueOf(1), list.getElement(1));
+    }
+
+    @Test
+    public void testRemoveAtt() {
+        list.removeAt(1);
+        Assert.assertEquals(3, list.getCount());
+        Assert.assertEquals(Integer.valueOf(0), list.getElement(0));
+        Assert.assertEquals(Integer.valueOf(0), list.getElement(1));
+        Assert.assertEquals(Integer.valueOf(2), list.getElement(2));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveAttException() {
+        list.removeAt(4);
+    }
+
+    @Test
+    public void testRemoveFirstEqualsObject() {
+        boolean isRemove = list.remove(0);
+        Assert.assertTrue(isRemove);
+        Assert.assertEquals(3, list.getCount());
+        Assert.assertEquals(Integer.valueOf(1), list.getElement(0));
+        Assert.assertEquals(Integer.valueOf(0), list.getElement(1));
+        Assert.assertEquals(Integer.valueOf(2), list.getElement(2));
+    }
+
+    @Test
+    public void testRemoveNonExistenObject() {
+        boolean isRemove = list.remove(5);
+        Assert.assertFalse(isRemove);
+        Assert.assertEquals(4, list.getCount());
+    }
+
+    @Test
+    public void testGetIndexOfValue() {
+        Assert.assertEquals(0, list.indexOf(0));
+    }
+
+    @Test
+    public void testGetNonExistenObject() {
+        Assert.assertEquals(-1, list.indexOf(3));
+    }
+
+    @Test
+    public void testGetElement() {
+        Assert.assertEquals(Integer.valueOf(1), list.getElement(1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetNonExistenElement() {
+        list.getElement(4);
+    }
+
+    @Test
+    public void testContains() {
+        Assert.assertTrue(list.contains(0));
+        Assert.assertFalse(list.contains(5));
+    }
+
+    @Test
+    public void testClear() {
+        list.clear();
+        Assert.assertEquals(0, list.getCount());
+        Assert.assertEquals(0, list.toArray().length);
+    }
+
+    @Test
+    public void testCopyTo() {
+        Integer[] array = new Integer[4];
+        list.copyTo(array, 0);
+        Assert.assertEquals(Integer.valueOf(0), array[0]);
+        Assert.assertEquals(Integer.valueOf(1), array[1]);
+        Assert.assertEquals(Integer.valueOf(0), array[2]);
+        Assert.assertEquals(Integer.valueOf(2), array[3]);
+    }
+
+    @Test
+    public void testGetCount() {
+        Assert.assertEquals(4, list.getCount());
+    }
+
+    @Test
+    public void testToArray() {
+        Object[] array = list.toArray();
+        Assert.assertEquals(4, array.length);
+        Assert.assertEquals(0, array[0]);
+        Assert.assertEquals(1, array[1]);
+        Assert.assertEquals(0, array[2]);
+        Assert.assertEquals(2, array[3]);
+
+    }
+}
